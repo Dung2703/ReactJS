8ea216e7d4d7986d3169fc03822ff5bbf26cd376
Tri Dung <ttdung10x@gmail.com>
2026-06-09 00:23:34 +0700

    start

---
+++ lesson/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+
+function App() {
+  let name = "Dung";
+  const css = {
+    color: "red",
+    backgroundColor: "blue"
+  }
+  return (
+    <>
+      <Header />
+      <div className="test" style={css}>
+        <h1>Hello, {name}!</h1>
+      </div>
+      <div className="test" style={css}>
+        <h1>Hello, {name}!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
